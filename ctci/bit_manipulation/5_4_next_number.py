2164f0852f825f8f59d7d06c81d764644a5bd1d5
Tahir Rauf <tahir.rauf1@gmail.com>
2016-12-28 00:28:51 -0800

    next number
    Signed-off-by: Tahir Rauf <tahir.rauf1@gmail.com>

--- ctci/bit_manipulation/5_4_next_number.py
+++ ctci/bit_manipulation/5_4_next_number.py
@@ -7,8 +7,16 @@ def next_number(num):
     bin_num = int(bin_str, 2)
     idx_of_right_most_non_trailing_zero = get_idx_of_right_most_non_trailing_zero(bin_num)
     number_of_ones_before_right_most_non_trailing_zero = get_number_of_ones_before_right_most_non_trailing_zero(bin_num)
-    bin_num = bit_manipulation.setbit(bin_num, idx_of_right_most_non_trailing_zero)
 
+    # Set idx bit
+    bin_num = bit_manipulation.setbit(bin_num, idx_of_right_most_non_trailing_zero)
+    # Clear all bits after idx bit
+    all_zeroes_followed_by_idx_ones = (1 << idx_of_right_most_non_trailing_zero) - 1
+    all_ones_followed_by_idx_zeroes = ~all_zeroes_followed_by_idx_ones
+    bin_num &= all_ones_followed_by_idx_zeroes
+    # Set least significant (number_of_ones_before_right_most_non_trailing_zero - 1) bits
+    mask = (1 << (number_of_ones_before_right_most_non_trailing_zero - 1)) - 1  # Number of p-1 zeroes followed by ones
+    return bin_num | mask
 
 
 def get_idx_of_right_most_non_trailing_zero(bin_num):
@@ -36,6 +44,7 @@ def get_number_of_ones_before_right_most_non_trailing_zero(bin_num):
 
     return -1
 
-print (get_idx_of_right_most_non_trailing_zero(int("100111010", 2)))
-print (get_number_of_ones_before_right_most_non_trailing_zero(int("100111010", 2)))
+# print (get_idx_of_right_most_non_trailing_zero(int("100111010", 2)))
+# print (get_number_of_ones_before_right_most_non_trailing_zero(int("100111010", 2)))
+print (next_number(5))
 
--- ctci/bit_manipulation/bit_manipulation.py
+++ ctci/bit_manipulation/bit_manipulation.py
@@ -3,20 +3,23 @@
 
 def setbit(bitmap, index):
     mask = (1 << index)
-    bitmap = bitmap | mask
+    return bitmap | mask
 
 
 def clearbit(bitmap, index):
     mask = ~(1 << index)
-    bitmap = bitmap & mask
+    return bitmap & mask
 
 
 def getbit(bitmap, index):
     mask = (1 << index)
     return ((bitmap & mask) != 0)
 
-def clear_bits_at_right_of_index(bitmap, idx):
-    mask = (1 << idx)
+
+def clear_bits_msb_through_idx(bitmap, idx):
+    mask = (1 << idx)  # All zeroes except 1 at position idx (idx= 2. 000100)
+    mask -= 1  # All zeroes followed by idx 1  (000011)
+    return bitmap & mask
 
 # https://www.willmcgugan.com/blog/tech/post/finding-the-first-bit-set-with-python/
 def getbit_index(bitmap):
